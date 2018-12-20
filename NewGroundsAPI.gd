268eae2615ce9e43e9b0a83f07de492ed53c8f78
Piotr Rojewski <rojek_abc@o2.pl>
2018-12-20 14:24:36 +0100

    Append App Component.
    This allow to use sessions.
    
    Also add reading of current session id from newgrounds iframe,
    which is needed to corelate with user/session.
    
    Change returned result to be whole data and also fix returning, when
    something wrong happends.

--- NewGroundsAPI.gd
+++ NewGroundsAPI.gd
@@ -8,16 +8,27 @@ export(String) var applicationId
 signal ng_request_complete
 
 var Gateway
+var ScoreBoard
+var App
 
-var _response
+var session_id
 
 func _ready():
 	use_threads = OS.get_name() != "HTML5"
+	
 	connect("request_completed", self, "_request_completed")
+	
 	Gateway = ComponentGateway.new(self)
+	ScoreBoard = ComponentScoreBoard.new(self)
+	App = ComponentApp.new(self)
+	
+	if OS.get_name() == 'HTML5':
+		session_id = JavaScript.eval('var urlParams = new URLSearchParams(window.location.search); urlParams.get("ngio_session_id")', true)
+		print('Session id: ' + str(session_id))
+		print('Location hostname: ' + str(JavaScript.eval('location.hostname')))
 	pass
 
-func _call_ng_api(component, function, parameters=null, debug=null, echo=null):
+func _call_ng_api(component, function, _session_id=null, parameters=null, debug=null, echo=null):
 	var headers = [
 		"Content-Type: application/x-www-form-urlencoded"
 	]
@@ -25,14 +36,19 @@ func _call_ng_api(component, function, parameters=null, debug=null, echo=null):
 	
 	requestData.app_id = applicationId
 	if debug:
-		requestData['debug'] = true
+		requestData.debug = true
+	if session_id:
+		requestData.session_id = _session_id
 	requestData.call = {}
 	requestData.call.component = component + '.' + function
+	requestData.call.parameters = parameters
 	
 	var requestJson = JSON.print(requestData)
 	if verbose:
 		print(requestJson)
-	request(NEW_GROUNDS_API_URL, headers, true, HTTPClient.METHOD_POST, 'input=' + requestJson.percent_encode())
+	var requestResult = request(NEW_GROUNDS_API_URL, headers, true, HTTPClient.METHOD_POST, 'input=' + requestJson.percent_encode())
+	if requestResult != OK:
+		emit_signal('ng_request_complete', {'error': 'Request result = ' + str(requestResult)})
 	pass
 	
 func _request_completed(result, response_code, headers, body):
@@ -40,19 +56,57 @@ func _request_completed(result, response_code, headers, body):
 	if verbose:
 		print('Response code: ' + str(response_code))
 		print('Response body: ' + responseBody)
+	if result != OK:
+		emit_signal('ng_request_complete', {'error': 'Response result = ' + str(result)})
+		return
+	if response_code >= 300:
+		emit_signal('ng_request_complete', {'error': 'Response status code = ' + str(response_code)})
+		return
+		
 	var jsonBody = JSON.parse(responseBody)
-	if jsonBody.error == OK:
-		if jsonBody.result.success:
-			match jsonBody.result.result.component:
-				'Gateway.ping': _response = jsonBody.result.result.data.pong
-				'Gateway.getVersion': _response = jsonBody.result.result.data.version
-				'Gateway.getDatetime': _response = jsonBody.result.result.data.datetime
-		else:
-			print(str(jsonBody.result.error.code) + ' ' + str(jsonBody.result.error.message))
-	else:
-		print('Wrong JSON body')
-	emit_signal('ng_request_complete', _response)
+	if jsonBody.error != OK:
+		emit_signal('ng_request_complete', {'error': 'Response has wrong JSON body'})
+		return
+	if !jsonBody.result.success:
+		emit_signal('ng_request_complete', {'error': 'New Grounds error: ' + str(jsonBody.result.error.code) + ' ' + str(jsonBody.result.error.message)})
+		return
+	if !jsonBody.result.result.data.success:
+		emit_signal('ng_request_complete', {'error': 'New Grounds data error: ' + str(jsonBody.result.result.data.error.code) + ' ' + str(jsonBody.result.result.data.error.message)})
+		return
+	var response = jsonBody.result.result.data
+	emit_signal('ng_request_complete', {'response': response, 'error': null})
 	pass
+
+class ComponentApp:
+	const NAME = 'App'
+	var api
+	func _init(_api):
+		api = _api
+		
+	func checkSession(sessionId=api.session_id):
+		api._call_ng_api(NAME, 'checkSession', sessionId)
+		pass
+		
+	func endSession(sessionId=api.session_id):
+		api._call_ng_api(NAME, 'endSession', sessionId)
+		pass
+		
+	func getCurrentVersion(version=null):
+		api._call_ng_api(NAME, 'getCurrentVersion', null, {'version' : version})
+		pass
+		
+	func getHostLicense(host=null):
+		api._call_ng_api(NAME, 'getHostLicense', null, {'host' : host})
+		pass
+		
+	func logView(host):
+		api._call_ng_api(NAME, 'logView', null, {'host' : host})
+		pass
+		
+	func startSession(force=null):
+		api._call_ng_api(NAME, 'startSession', null, {'force' : force})
+		pass
+	
 
 class ComponentGateway:
 	const NAME = 'Gateway'
@@ -72,3 +126,11 @@ class ComponentGateway:
 		api._call_ng_api(NAME, 'ping')
 		pass
 
+class ComponentScoreBoard:
+	const NAME = 'ScoreBoard'
+	var api
+	func _init(_api):
+		api = _api
+	func getBoards():
+		api._call_ng_api(NAME, 'getBoards')
+		pass
