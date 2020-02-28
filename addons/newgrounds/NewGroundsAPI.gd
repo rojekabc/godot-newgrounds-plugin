35f612ff063e1deb102a29c6f6807c9d63c625c9
AniMerrill <ethan@animerrill.com>
2020-02-28 12:54:52 -0500

    Style Guide Update; Changed to GLES 2
    Made a lot of changes to all the script files to make them better
    conform to the current GDScript style guide recommendations as
    well as general readability updates. Attempted to add stronger
    typing in a few places, and reordered the api request function
    to be less verbose in the most cases (i.e. parameters and
    session_id were swapped). Everything still functions as
    intended as far as I can tell.
    
    Also changed the renderer for the project to GLES 2 because the
    HTML5 versions of games can't really take advantage of GLES 3
    and for highest usability developers should stick to GLES 2 for
    web games anyway.
    
    Things to note:
    - Classes for components should probably be moved to their own
    script files and I may do that myself if I have a chance to do
    so.
    - Attempted to add debug as an export variable to the API since
    there is no chance to call the API request anywhere in the
    original code with the debug parameter. This would theoretically
    make it possible for the end user developer to simply click a
    checkbox on or off for testing/release, but the debug parameter
    doesn't seem to work properly (at least according to how the
    NGio docs say it should).
    - Separated script of the Sample scene to it's own file. It might
    even be most desirable to make separate scripts for each button
    so that each function can be viewed in isolation, right now the
    sample code is kind of a mess to try and read.

--- addons/newgrounds/NewGroundsAPI.gd
+++ addons/newgrounds/NewGroundsAPI.gd
@@ -1,212 +1,356 @@
 extends HTTPRequest
 
-const NEW_GROUNDS_API_URL = 'https://www.newgrounds.io/gateway_v3.php'
 
-export(bool) var verbose
-export(String) var applicationId
+const NEW_GROUNDS_API_URL = "https://www.newgrounds.io/gateway_v3.php"
 
 signal ng_request_complete
 
-var App
-var Event
-var Gateway
-var Loader
-var Medal
-var ScoreBoard
+export var applicationId := ""
+export var verbose := false
+export var debug := false
 
-var session_id
+var App : ComponentApp
+var Event : ComponentEvent
+var Gateway : ComponentGateway
+var Loader : ComponentLoader
+var Medal : ComponentMedal
+var ScoreBoard : ComponentScoreBoard
 
-func is_ok(ngResult):
-	return ngResult != null and ngResult.error == null
+var session_id : String
+
 
 func _ready():
 	use_threads = OS.get_name() != "HTML5"
 	
 	connect("request_completed", self, "_request_completed")
 	
-	Gateway = ComponentGateway.new(self)
-	ScoreBoard = ComponentScoreBoard.new(self)
 	App = ComponentApp.new(self)
 	Event = ComponentEvent.new(self)
+	Gateway = ComponentGateway.new(self)
 	Loader = ComponentLoader.new(self)
 	Medal = ComponentMedal.new(self)
+	ScoreBoard = ComponentScoreBoard.new(self)
 	
-	if OS.get_name() == 'HTML5':
-		session_id = JavaScript.eval('var urlParams = new URLSearchParams(window.location.search); urlParams.get("ngio_session_id")', true)
-		_verbose('Session id: ' + str(session_id))
-		_verbose('Location hostname: ' + str(JavaScript.eval('location.hostname')))
-	pass
+	if OS.get_name() == "HTML5":
+		session_id = JavaScript.eval(
+				"""
+				var urlParams = new URLSearchParams(window.location.search); 
+				urlParams.get('ngio_session_id')
+				""", 
+				true)
+		
+		_verbose("Session id: " + str(session_id))
+		_verbose("Location hostname: " + str(JavaScript.eval("location.hostname")))
 
-func _call_ng_api(component, function, _session_id=null, parameters=null, debug=null, echo=null):
-	var headers = [
-		"Content-Type: application/x-www-form-urlencoded"
-	]
-	var requestData = {}
+
+func _call_ng_api(
+		component : String,
+		function : String, 
+		parameters := {},  
+		_session_id := "",
+		echo := ""
+		):
 	
-	if applicationId:
+	var headers := [ "Content-Type: application/x-www-form-urlencoded" ]
+	var requestData := {}
+	var requestJson : String
+	var requestResult : int # @GlobalScope.Error
+	
+	if applicationId != "":
 		requestData.app_id = applicationId
+	
 	if debug:
 		requestData.debug = true
-	if session_id:
+	
+	if session_id != "":
 		requestData.session_id = _session_id
+	
 	requestData.call = {}
 	requestData.call.component = component + '.' + function
 	requestData.call.parameters = parameters
 	
-	var requestJson = JSON.print(requestData)
-	_verbose(requestJson)
-	var requestResult = request(NEW_GROUNDS_API_URL, headers, true, HTTPClient.METHOD_POST, 'input=' + requestJson.percent_encode())
-	if requestResult != OK:
-		emit_signal('ng_request_complete', {'error': 'Request result = ' + str(requestResult)})
-	pass
+	requestJson = JSON.print(requestData)
 	
-func _request_completed(result, response_code, headers, body):
-	var responseBody = body.get_string_from_utf8()
-	_verbose(responseBody)
-	if result != OK:
-		emit_signal('ng_request_complete', {'error': 'Response result = ' + str(result)})
-		return
-	if response_code >= 300:
-		emit_signal('ng_request_complete', {'error': 'Response status code = ' + str(response_code)})
-		return
-		
-	var jsonBody = JSON.parse(responseBody)
-	if jsonBody.error != OK:
-		emit_signal('ng_request_complete', {'error': 'Response has wrong JSON body'})
-		return
-	if !jsonBody.result.success:
-		emit_signal('ng_request_complete', {'error': 'New Grounds error: ' + str(jsonBody.result.error.code) + ' ' + str(jsonBody.result.error.message)})
-		return
-	if !jsonBody.result.result.data.success:
-		emit_signal('ng_request_complete', {'error': 'New Grounds data error: ' + str(jsonBody.result.result.data.error.code) + ' ' + str(jsonBody.result.result.data.error.message)})
-		return
-	var response = jsonBody.result.result.data
-	emit_signal('ng_request_complete', {'response': response, 'error': null})
-	pass
+	_verbose(requestJson)
+	
+	requestResult = request(
+			NEW_GROUNDS_API_URL, 
+			headers, 
+			true, 
+			HTTPClient.METHOD_POST, 
+			'input=' + requestJson.percent_encode()
+			)
+	
+	if requestResult != OK:
+		emit_signal(
+			'ng_request_complete', 
+			{'error': 'Request result = ' + str(requestResult)}
+			)
 
-func _verbose(msg):
+
+func _request_completed(
+		result : int, 
+		response_code : int, 
+		headers : PoolStringArray, 
+		body : PoolByteArray
+		):
+	
+	var responseBody := body.get_string_from_utf8()
+	var jsonBody : JSONParseResult
+	var response # TODO: Add typing
+	
+	_verbose(responseBody)
+	
+	if result != OK:
+		emit_signal(
+				'ng_request_complete', 
+				{'error': 'Response result = ' + str(result)}
+				)
+		return
+	
+	if response_code >= 300:
+		emit_signal(
+				'ng_request_complete', 
+				{'error': 'Response status code = ' + str(response_code)}
+				)
+		
+		return
+	
+	jsonBody = JSON.parse(responseBody)
+	
+	if jsonBody.error != OK:
+		emit_signal(
+				'ng_request_complete', 
+				{'error': 'Response has wrong JSON body'}
+				)
+		
+		return
+	
+	if !jsonBody.result.success:
+		emit_signal(
+				'ng_request_complete', 
+				{
+					'error': 'New Grounds error: ' + \
+					str(jsonBody.result.error.code) + ' ' + \
+					str(jsonBody.result.error.message)
+					}
+				)
+		
+		return
+	
+	if !jsonBody.result.result.data.success:
+		emit_signal(
+				'ng_request_complete', 
+				{
+					'error': 'New Grounds data error: ' + \
+					str(jsonBody.result.result.data.error.code) + ' ' + \
+					str(jsonBody.result.result.data.error.message)
+					}
+				)
+		
+		return
+	
+	response = jsonBody.result.result.data
+	
+	emit_signal('ng_request_complete', {'response': response, 'error': null})
+
+
+func _verbose(msg : String):
 	if verbose:
 		print('[NG Plugin] ' + msg)
 
+
+func is_ok(ngResult) -> bool:
+	return ngResult != null and ngResult.error == null
+
+
+# TODO: Potentially move these into their own files and give everything a class_name
 class ComponentLoader:
 	const NAME = 'Loader'
 	var api
+	
+	
 	func _init(_api):
 		api = _api
 	
+	
 	func loadAuthorUrl(host, redirect=false):
-		api._call_ng_api(NAME, 'loadAuthorUrl', null, {'host' : host, 'redirect' : redirect})
-		pass
-		
+		api._call_ng_api(
+				NAME, 
+				'loadAuthorUrl', 
+				{'host' : host, 'redirect' : redirect}
+				)
+	
+	
 	func loadMoreGames(host, redirect=false):
-		api._call_ng_api(NAME, 'loadMoreGames', null, {'host' : host, 'redirect' : redirect})
-		pass
-		
+		api._call_ng_api(
+				NAME, 
+				'loadMoreGames', 
+				{'host' : host, 'redirect' : redirect}
+				)
+	
+	
 	func loadNewgrounds(host, redirect=false):
-		api._call_ng_api(NAME, 'loadNewgrounds', null, {'host' : host, 'redirect' : redirect})
-		pass
-		
+		api._call_ng_api(
+				NAME, 
+				'loadNewgrounds',
+				{'host' : host, 'redirect' : redirect}
+				)
+	
+	
 	func loadOfficialUrl(host, redirect=false):
-		api._call_ng_api(NAME, 'loadOfficialUrl', null, {'host' : host, 'redirect' : redirect})
-		pass
-		
+		api._call_ng_api(
+				NAME, 
+				'loadOfficialUrl', 
+				{'host' : host, 'redirect' : redirect}
+				)
+	
+	
 	func loadReferral(host, referral_name, redirect=false):
-		api._call_ng_api(NAME, 'loadReferral', null, {'host' : host, 'referral_name' : referral_name, 'redirect' : redirect})
-		pass
+		api._call_ng_api(
+				NAME, 
+				'loadReferral', 
+				{
+					'host' : host, 
+					'referral_name' : referral_name, 
+					'redirect' : redirect
+					}
+				)
+
 
 class ComponentMedal:
 	const NAME = 'Medal'
 	var api
+	
+	
 	func _init(_api):
 		api = _api
-		
+	
+	
 	func getList(sessionId=api.session_id):
-		api._call_ng_api(NAME, 'getList', sessionId)
-		pass
+		api._call_ng_api(NAME, 'getList', {}, sessionId)
+	
+	
 	
 	func unlock(medalId, sessionId=api.session_id):
-		api._call_ng_api(NAME, 'unlock', sessionId, {'id' : medalId})
-		pass
+		api._call_ng_api(NAME, 'unlock', {'id' : medalId}, sessionId)
+
 
 class ComponentEvent:
 	const NAME = 'Event'
 	var api
+	
+	
 	func _init(_api):
 		api = _api
-		
+	
+	
 	func logEvent(host, event_name):
-		api._call_ng_api(NAME, 'logEvent', null, {'host' : host, 'event_name' : event_name})
-		pass
-		
+		api._call_ng_api(
+				NAME, 
+				'logEvent', 
+				{'host' : host, 'event_name' : event_name}
+				)
+
 
 class ComponentApp:
 	const NAME = 'App'
 	var api
+	
+	
 	func _init(_api):
 		api = _api
-		
-	func checkSession(sessionId=api.session_id):
-		api._call_ng_api(NAME, 'checkSession', sessionId)
-		pass
-		
-	func endSession(sessionId=api.session_id):
-		api._call_ng_api(NAME, 'endSession', sessionId)
-		pass
-		
-	func getCurrentVersion(version=null):
-		api._call_ng_api(NAME, 'getCurrentVersion', null, {'version' : version})
-		pass
-		
-	func getHostLicense(host=null):
-		api._call_ng_api(NAME, 'getHostLicense', null, {'host' : host})
-		pass
-		
-	func logView(host):
-		api._call_ng_api(NAME, 'logView', null, {'host' : host})
-		pass
-		
-	func startSession(force=null):
-		api._call_ng_api(NAME, 'startSession', null, {'force' : force})
-		pass
 	
+	
+	func checkSession(sessionId=api.session_id):
+		api._call_ng_api(NAME, 'checkSession', {}, sessionId)
+	
+	
+	func endSession(sessionId=api.session_id):
+		api._call_ng_api(NAME, 'endSession', {}, sessionId)
+	
+	
+	func getCurrentVersion(version=null):
+		api._call_ng_api(NAME, 'getCurrentVersion', {'version' : version})
+	
+	
+	func getHostLicense(host=null):
+		api._call_ng_api(NAME, 'getHostLicense', {'host' : host})
+	
+	
+	func logView(host):
+		api._call_ng_api(NAME, 'logView', {'host' : host})
+	
+	
+	func startSession(force=null):
+		api._call_ng_api(NAME, 'startSession', {'force' : force})
+
 
 class ComponentGateway:
 	const NAME = 'Gateway'
 	var api
+	
+	
 	func _init(_api):
 		api = _api
-
+	
+	
 	func getVersion():
 		api._call_ng_api(NAME, 'getVersion')
-		pass
-		
+	
+	
 	func getDatetime():
 		api._call_ng_api(NAME, 'getDatetime')
-		pass
-		
+	
+	
 	func ping():
 		api._call_ng_api(NAME, 'ping')
-		pass
+
 
 class ComponentScoreBoard:
 	const NAME = 'ScoreBoard'
 	var api
 	
+	
 	func _init(_api):
 		api = _api
 	
+	
 	func getBoards():
 		api._call_ng_api(NAME, 'getBoards')
-		pass
 	
-	func getScores(scoreId, sessionId=api.session_id, limit=10, skip=0, social=false, tag=null, period=null, userId=null):
-		api._call_ng_api(NAME, 'getScores', sessionId,
-			{
-				'id' : scoreId, 'limit': limit, 'skip': skip, 'social': social, 'tag': tag,
-				'period': period, 'userId': userId
-			})
-		pass
-
+	
+	func getScores(
+			scoreId, 
+			sessionId=api.session_id, 
+			limit=10, 
+			skip=0, 
+			social=false, 
+			tag=null, 
+			period=null, 
+			userId=null
+			):
+		
+		api._call_ng_api(
+				NAME, 
+				'getScores', 
+				{
+					'id' : scoreId, 
+					'limit': limit, 
+					'skip': skip, 
+					'social': social, 
+					'tag': tag,
+					'period': period, 
+					'userId': userId
+					},
+				sessionId
+				)
+	
+	
 	func postScore(value, scoreId, sessionId=api.session_id, tag=null):
-		api._call_ng_api(NAME, 'postScore', sessionId, {'value' : value, 'id': scoreId, 'tag': tag})
-		pass
+		api._call_ng_api(
+				NAME, 
+				'postScore', 
+				{'value' : value, 'id': scoreId, 'tag': tag},
+				sessionId
+				)
